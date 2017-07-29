d97f6581d26aba0b95cd202c571d630e2e410431
Eric Coleman <eric@epiccoleman.com>
2017-07-28 21:42:37 -0400

    implement 'too high' message

--- web_guesser.rb
+++ web_guesser.rb
@@ -2,7 +2,14 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 secret_number = rand 100
+message = "nothing"
 
 get '/' do
-  erb :index, :locals => { :secret_number => secret_number }
+  if params['guess'].to_i > secret_number
+    message = "too high"
+  end
+
+  erb :index, :locals => { :secret_number => secret_number,
+                           :message => message }
+
 end

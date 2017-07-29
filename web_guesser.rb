26567645129807ed388461201c09e0323edf2154
Eric Coleman <eric@epiccoleman.com>
2017-07-28 21:49:22 -0400

    implement 'too low' and 'you got it' messages

--- web_guesser.rb
+++ web_guesser.rb
@@ -2,11 +2,15 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 secret_number = rand 100
-message = "nothing"
+message = ""
 
 get '/' do
   if params['guess'].to_i > secret_number
-    message = "too high"
+      message = "Your guess is too high."
+  elsif params['guess'].to_i < secret_number
+      message = "Your guess is too low."
+  elsif params['guess'].to_i == secret_number
+      message = "You guessed the secret number!"
   end
 
   erb :index, :locals => { :secret_number => secret_number,
